d6637c80b28257dff6bd12f6d9a084377fd79400
bakrueger75 <37382462+bakrueger75@users.noreply.github.com>
2018-09-24 21:31:54 -0500

    Refactor Updates

--- dogfinder/src/components/DogSearchItem.js
+++ dogfinder/src/components/DogSearchItem.js
@@ -30,10 +30,10 @@ export default class DogSearchItem  extends React.Component {
   render() {
     var dogClass = "col-md";
  		if (this.props.dogCount >= 3) {
-	 		dogClass += "col-md-3";
+	 		dogClass = "col-md-4";
 		}
     return (
-      <div id={"dogBreed_"+this.props.itemIndex} className="{dogClass} dogSearchItem" key={this.props.itemIndex} onClick={() => {this.fetchImage(this.props.dog.breed, this.props.dog.subBreed)}} >
+      <div id={"dogBreed_"+this.props.itemIndex} className={dogClass + ' dogSearchItem'} key={this.props.itemIndex} onClick={() => {this.fetchImage(this.props.dog.breed, this.props.dog.subBreed)}} >
         <img className="dogResultImg" src={this.state.imageUrl} alt={this.props.breedName}/>
         <div id={"dogBreedName_"+this.props.itemIndex} ><h2 className="text-capitalize text-red">{this.props.dog.breedName}</h2></div>
       </div>
