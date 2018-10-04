c309ce11ab83127de0c6709d191e7656dd9cb1a8
Brian Krueger <briankrueger@northwesternmutual.com>
2018-10-04 15:06:13 -0500

    Adjusted Routes

--- dogfinder/src/App.js
+++ dogfinder/src/App.js
@@ -1,17 +1,9 @@
-//import React, { Component } from 'react';
 import React from 'react';
-//import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import DogFinder from './components/DogFinder';
-import DogDetail from './components/DogDetail/DogDetail';
-import AboutPage from './components/about/aboutPage.js';
+import routes from './routes.js';
 
 const App = () => (
   <div>
-      <Route exact path="/" component={DogFinder} />
-      <Route exact path="/detail/:breed" component={DogDetail} />
-      <Route exact path="/detail/:breed/:subBreed" component={DogDetail} />
-      <Route exact path="/about" component={AboutPage} />
+    {routes}
   </div>
 );
 
